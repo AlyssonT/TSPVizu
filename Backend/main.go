f56051f0c2b54ad76033b8c5c877de46cbcf61f1
Alysson Cruz <alyssontccruz@gmail.com>
2025-09-03 18:20:47 -0300

    refac: removed unused comments

--- Backend/main.go
+++ Backend/main.go
@@ -21,47 +21,4 @@ func main() {
 	server.POST("/solve", handlers.TSP_Solve)
 
 	server.Run()
-
-	// instance := tsp.NewTSPInstanceFromFile("st70.tsp")
-
-	// sequential := handlers.Range(instance.Cities)
-	// ils := tsp.NewILS(instance)
-	// bestEval := math.MaxInt
-	// bestSolution := make([]int, len(instance.Cities))
-
-	// start := time.Now()
-	// type Result struct {
-	// 	Solution []int
-	// 	Eval     int
-	// }
-
-	// resultChan := make(chan Result, 2500)
-
-	// for range 2500 {
-	// 	go func() {
-	// 		solution, eval := ils.Solve(sequential)
-	// 		resultChan <- Result{Solution: solution, Eval: eval}
-	// 	}()
-	// }
-
-	// for range 2500 {
-	// 	result := <-resultChan
-	// 	if result.Eval < bestEval {
-	// 		bestEval = result.Eval
-	// 		bestSolution = result.Solution
-	// 	}
-	// }
-
-	// elapsed := time.Since(start)
-
-	// fmt.Println(elapsed)
-	// fmt.Println(bestSolution, bestEval)
-
-	// aco := tsp.NewACO(instance, 10)
-	// start = time.Now()
-	// aco.Solve(1.0, 5.0, 0.96, 0.1, 30000, 100)
-	// elapsed = time.Since(start)
-
-	// fmt.Println(elapsed)
-	// fmt.Println(aco.BestTrail, aco.BestEval)
 }
